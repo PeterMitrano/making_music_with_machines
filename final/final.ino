2d6c3c697b2eee07ab766dbc417a497d7bbfaefe
Peter Mitrano <mitranopeter@gmail.com>
2016-10-13 02:51:48 -0400

    add meander functionality
    messed with the constants some more

--- final/final.ino
+++ final/final.ino
@@ -8,13 +8,31 @@ Adafruit_DCMotor *motor = AFMS.getMotor(1);
 Encoder flag_encoder(2, 4);
 Encoder motor_encoder(3, 5);
 
-const float kP = -.05;
-const int max_speed = 30;
+const float kP = -.03;
+const int max_speed = 20, min_speed = 10;
 const float flag_to_motor = 16 * (3200.0 / 360.0);
+const int meander_step = 20;
+const int meander_interval = 2000;
+long current_target;
+unsigned long last_meander = millis();
 
 void setup() {
   Serial.begin(9600);
   AFMS.begin();
+}
+
+long meander(long flag_ticks) {
+  long meander_output = flag_ticks;
+  float r = random(0, 100) / 100.0;
+  if (r < 0.2) {
+    meander_output += random(0 , meander_step);
+  }
+  else if (r < 0.4) {
+    meander_output -= random(0 , meander_step);
+  }
+
+
+  return meander_output;
 }
 
 void loop() {
@@ -22,23 +40,25 @@ void loop() {
   long flag_ticks = flag_encoder.read();
   long current_motor_ticks = -motor_encoder.read();
 
-  long target_motor_ticks = flag_to_motor * flag_ticks;
+  unsigned long now = millis();
+  if (now - last_meander > meander_interval) {
+    current_target = meander(flag_ticks);
+    last_meander = now;
+  }
 
-  Serial.print(current_motor_ticks);
-  Serial.print(" ");
-  Serial.print(target_motor_ticks);
-  Serial.print(" ");
+
+  long target_motor_ticks = flag_to_motor * current_target;
 
   int output = kP * (current_motor_ticks - target_motor_ticks);
 
   output = min(output, max_speed);
   output = max(output, -max_speed);
 
-  if (output > 4) {
+  if (output > min_speed) {
     motor->run(FORWARD);
-     motor->setSpeed(output);
+    motor->setSpeed(output);
   }
-  else if (output < -4) {
+  else if (output < -min_speed) {
     motor->setSpeed(-output);
     motor->run(BACKWARD);
   }
@@ -46,4 +66,3 @@ void loop() {
     motor->run(RELEASE);
   }
 }
-
